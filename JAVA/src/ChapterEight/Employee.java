31cb0e9f1176e4b528f13cad6044930df4797010
Zeuxx <isaac.ussery@hotmail.com>
2017-08-22 23:37:59 -0400

    IntelliJ Test

---
+++ JAVA/src/ChapterEight/Employee.java
@@ -0,0 +1,22 @@
+package ChapterEight;
+
+public class Employee
+{
+    private int empNum;
+    private double empSal;
+    Employee(int e, double s)
+    {
+        empNum = e;
+        empSal = s;
+    }
+
+    public int getEmpNum()
+    {
+        return empNum;
+    }
+
+    public double getEmpSal()
+    {
+        return empSal;
+    }
+}
